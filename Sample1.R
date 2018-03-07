72c1b0b31072469ddd7ecbf662f465a4c537cbca
msatyan <msatyan@hcl.com>
2018-03-07 12:11:16 -0600

    R

--- Sample1.R
+++ Sample1.R
@@ -1,6 +1,6 @@
 
 library(IfxR)
-ch <- IfxConnect( "SERVER=ids0;DATABASE=db1;HOST=127.0.0.1;SERVICE=9088;UID=informix;PWD=Blue4You;" )
+ch <- IfxConnect( "SERVER=ids0;DATABASE=db1;HOST=127.0.0.1;SERVICE=9088;UID=informix;PWD=xxx;" )
 tmp <- IfxQuery(ch, "select * from t1") 
 res <- IfxFetchRows(ch)
 print (res)
